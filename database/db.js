1bc59d74808df065150a576cd5c132a12c1e34ec
AlbertoLcm <alberto.cm4007@gmail.com>
2022-06-13 00:02:22 -0500

    cada vez menos errores

--- database/db.js
+++ database/db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
-const conexion = mysql.createConnection({
+const conexion = mysql.createPool({
     host: 'us-cdbr-east-05.cleardb.net',
     database: 'heroku_e3ce238d1d7757e',
     user: 'bfa774d190b3e6',
-    password: 'a429144e'
+    password: 'a429144e',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 conexion.connect((err) => {
